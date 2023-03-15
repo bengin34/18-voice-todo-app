3043953df3b85efcc7653ed5d2ba38672036adf8
bengin34 <becaglar3434@gmail.com>
2023-03-15 20:40:45 +0300

    cloud firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import Todo from "./components/Todo";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
+import {app, database} from './firebase-config'
+import { collection, addDoc } from 'firebase/firestore';
 
 
 function App() {
+  const databaseRef = collection(database, 'todo-list');
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: (commandData) => {
+        addDoc(databaseRef, { item: commandData.data })
+      
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Todo />
+    <div>
+      <Todo databaseRef={databaseRef}/>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { FiX } from "react-icons/fi";
 const Todo = () => {
   return (
-    <div>
-      <h2>Voice-based Todo Application</h2>
+    <div className="flex justify-center items-center flex-col">
+      <h2 className="text-2xl font- text-center mt-6">
+        Voice-based Todo Application
+      </h2>
+      <div className="border-dashed border-2 mt-5 border-gray-300 w-[50vh] h-[50vh] rounded-2xl ">
+        <div className=" flex justify-center items-center text-center mt-5">
+          <h3>Wash the Clothes</h3>
+          <FiX className="mx-3" />
+        </div>
+        <div className="flex justify-center items-center text-center mt-5">
+          <h3>Cook dinner</h3>
+          <FiX className="mx-3" />
+        </div>
+        <div className="flex justify-center items-center text-center mt-5">
+          <h3>Code more code</h3>
+          <FiX className="mx-3" />
+        </div>
+      </div>
     </div>
   );
 };
