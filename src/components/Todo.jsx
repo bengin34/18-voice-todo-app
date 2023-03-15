01b5c478119f691df4b67264635e4b1748885bdc
bengin34 <becaglar3434@gmail.com>
2023-03-16 01:39:22 +0300

    beta

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -26,14 +26,14 @@ const Todo = ({ databaseRef, update, setUpdate }) => {
   return (
     <div className="flex justify-center h-[100vh]  bg-gray-800 items-center flex-col">
       <h2 className="text-3xl font- text-center mt-6 text-[#ff2625] opacitiy-5">
-        Voice-based Todo Application
+      Say "Add ..." ( e.g. "Add study React")
       </h2>
-      <div className=" border-4 mt-5 border-pink-600 w-[50vh] h-[50vh] rounded-2xl ">
+      <div className=" border-4 mt-5 border-pink-600 bg-gray-200  w-[50vh] h-[50vh] rounded-2xl ">
         {todoList.map((todo) => {
           return (
             <div
               key={todo.id}
-              className=" flex justify-center items-center text-center mt-5"
+              className=" flex justify-center items-center text-center  mt-5"
             >
               <h3 className="text-xl" >{todo.item}</h3>
               <FiX className="mx-3" onClick={() => deleteItems(todo.id)} />
